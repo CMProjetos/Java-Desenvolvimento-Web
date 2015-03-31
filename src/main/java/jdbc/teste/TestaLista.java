02a7f92108599ecc2412f835ab53e87cc9cf5903
carlos-sagarana <carloseduardo@sagaranatech.com>
2015-03-31 13:54:46 -0300

    Implementando pesquisa de dados na tabela contatos

---
+++ src/main/java/jdbc/teste/TestaLista.java
@@ -0,0 +1,33 @@
+package jdbc.teste;
+
+import jdbc.dao.ContatoDao;
+import jdbc.modelo.ConnectionFactory;
+import jdbc.modelo.Contato;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by carlos on 31/03/15.
+ */
+public class TestaLista {
+
+    public static void main(String[] args) {
+
+        ContatoDao dao = new ContatoDao();
+        List<Contato> contatos = dao.getLista();
+
+        for (Contato contato : contatos) {
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Email: " + contato.getEmail());
+            System.out.println("Endereço: " + contato.getEndereco());
+            System.out.println("Data de Nascimento: " + new SimpleDateFormat().format(contato.getDataNascimento().getTime()) + "\n");
+        }
+
+    }
+
+}
